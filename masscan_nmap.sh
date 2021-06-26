eec8c675acd885f9d9f0d043c133e8c2ff81bdd1
the3d3n <48614524+the3d3n@users.noreply.github.com>
2021-06-26 08:49:05 -0400

    Add files via upload

---
+++ masscan_nmap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo docker run -it --network host --rm adarnimrod/masscan -p1-65535 $1 --wait 0 -e tun0 --rate=1000>masscan.txt
+cat masscan.txt | grep -ohe "[0-9]*\/tcp" | awk -F / '{print $1}' > open_ports.txt
+var=$(cat open_ports.txt | sed -z 's/\n/,/g;s/,$/\n/')
+nmap -T4 -A -p $var $1 -oN nmap.enum
