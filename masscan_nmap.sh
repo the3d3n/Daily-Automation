b38d802ed497bf83ff9700a614623ba0f45862a5
the3d3n <batra.anirudh4@gmail.com>
2021-07-01 14:31:36 -0400

    Adding some changes

--- masscan_nmap.sh
+++ masscan_nmap.sh
@@ -2,5 +2,8 @@
 
 sudo docker run -it --network host --rm adarnimrod/masscan -p1-65535 $1 --wait 0 -e tun0 --rate=1000>masscan.txt
 cat masscan.txt | grep -ohe "[0-9]*\/tcp" | awk -F / '{print $1}' > open_ports.txt
+#We can also use tr 
+# tr '\n' ','
+#Will replace the new line character with comma
 var=$(cat open_ports.txt | sed -z 's/\n/,/g;s/,$/\n/')
 nmap -T4 -A -p $var $1 -oN nmap.enum
